1532c445612623cbb51472319664119e9f383eb0
Cheryl Lee <yli.yanchen@gmail.com>
2024-05-29 23:20:47 -0700

    setup tailwind and script

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/**/*.ts', './client/**/*.tsx', './**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
