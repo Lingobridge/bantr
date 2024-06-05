18c4b47ba910aca7b2b496a1d5d75d8976c0ba88
Cheryl Lee <yli.yanchen@gmail.com>
2024-06-04 22:41:19 -0700

    basic chat room UI setup

--- src/app/api/room/route.ts
+++ src/app/api/room/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { v4 as uuidV4 } from 'uuid';
 
 export async function POST(req: NextRequest) {
-  const roomId = uuidV4(); // Generate a unique room ID
+  const roomId = uuidV4().replace(/-/g, '').substring(0, 12); // Generate a unique room ID
   console.log('*** Room ID:', roomId);
   return NextResponse.json({ roomId });
 }
--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,28 +1,55 @@
 'use client';
 
-import { io, Socket } from "socket.io-client";
-import React, {useState, useEffect, useRef} from "react";
-import { useParams } from "next/navigation";
+import { io, Socket } from 'socket.io-client';
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from 'next/navigation';
+import { AiOutlineAudio } from 'react-icons/ai';
+import { MdAddCircleOutline } from 'react-icons/md';
+import { AiTwotoneSmile } from 'react-icons/ai';
+import { AiOutlineBars } from 'react-icons/ai';
 
-export default function Room () {
-    const socket = useRef<Socket | null>(null);
-    const params = useParams<{ id: string }>();
-  
-    useEffect(() => {
-        //create new websocket connection with host server and set socket to new socket instance
-        socket.current = io(); 
+import { Input } from '@/lib/ui/input';
 
-        return () => {
-            //disconnect socket when Room unmounts
-            if (socket.current) socket.current.disconnect();
-        }
-    }, []);
+export default function Room() {
+  const socket = useRef<Socket | null>(null);
+  const params = useParams<{ id: string }>();
+  const messageRef = useRef<HTMLInputElement>(null);
 
-    return (
-        <h1></h1>
-    );
+  useEffect(() => {
+    //create new websocket connection with host server and set socket to new socket instance
+    socket.current = io();
+
+    return () => {
+      //disconnect socket when Room unmounts
+      if (socket.current) socket.current.disconnect();
+    };
+  }, []);
+
+  return (
+    <main>
+      <div className='h-12 w-full flex flex-row items-center bg-slate-200 border'>
+        <div className='flex grow items-center justify-center'>
+          <div className='font-base text-lg'>Group Chat Room</div>
+        </div>
+        <div className='fixed right-4 w-12 justify-end'>
+          <AiOutlineBars className='w-10 text-center text-2xl justify-end' />
+        </div>
+      </div>
+
+      <div className='h-96'></div>
+
+      <div className='fixed bottom-0 h-16 w-full flex flex-row justify-center items-center bg-slate-200 border'>
+        <AiOutlineAudio className='w-12 text-center text-2xl mr-2' />
+        <Input
+          id='messageid'
+          type='text'
+          ref={messageRef}
+          className='w-full border-t overflow-y-auto'
+          placeholder='Type a message...'
+        ></Input>
+        <AiTwotoneSmile className='w-10 text-center text-2xl ml-2' />
+        <MdAddCircleOutline className='w-10 text-center text-2xl' />
+      </div>
+    </main>
+  );
 }
-
-
-
-
